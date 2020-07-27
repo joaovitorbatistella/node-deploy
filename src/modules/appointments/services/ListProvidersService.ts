dd8a3408df6f61cb650e0ebcbbd5881fedfaf199
João Vítor Batistella <joaovitor.batistella.ifrs@gmail.com>
2020-07-27 20:32:47 -0300

    finish

--- src/modules/appointments/services/ListProvidersService.ts
+++ src/modules/appointments/services/ListProvidersService.ts
@@ -1,5 +1,7 @@
 import { injectable, inject } from 'tsyringe';
 
+import { classToClass } from 'class-transformer';
+
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 import IUserRepository from '@modules/users/repositories/IUsersRepository';
 
@@ -29,7 +31,10 @@ class ListProvidersService {
         except_user_id: user_id,
       });
 
-      await this.caheProvider.save(`providers-list:${user_id}`, users);
+      await this.caheProvider.save(
+        `providers-list:${user_id}`,
+        classToClass(users),
+      );
     }
 
     return users;
